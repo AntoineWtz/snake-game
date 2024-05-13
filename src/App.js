d29d59dc513450b6cdccdc06a1ae0a21ee73961d
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-05-13 17:04:46 +0200

    ok clean test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import GameDetails from './components/GameDetails';
 import PlayBoard from './components/PlayBoard';
@@ -21,10 +21,38 @@ function App() {
       setHighScore(parseInt(savedHighScore));
     }
 
+    const moveSnake = () => {
+      if (gameOver) return handleGameOver();
+      let newSnake = [...snake];
+      let snakeX = newSnake[0][0];
+      let snakeY = newSnake[0][1];
+      snakeX += velocityX;
+      snakeY += velocityY;
+
+      if (
+        snakeX < 1 ||
+        snakeX > 30 ||
+        snakeY < 1 ||
+        snakeY > 30 ||
+        newSnake.slice(1).some(([x, y]) => x === snakeX && y === snakeY)
+      ) {
+        setGameOver(true);
+        return;
+      }
+
+      newSnake.unshift([snakeX, snakeY]);
+      if (snakeX === foodX && snakeY === foodY) {
+        setScore(score + 1);
+      } else {
+        newSnake.pop();
+      }
+      setSnake(newSnake);
+    };
+
     const id = setInterval(moveSnake, 100);
     setIntervalId(id);
     return () => clearInterval(id);
-  }, []);
+  }, [gameOver, snake, foodX, foodY, score, velocityX, velocityY]);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -78,34 +106,6 @@ function App() {
       setVelocityX(1);
       setVelocityY(0);
     }
-  };
-
-  const moveSnake = () => {
-    if (gameOver) return handleGameOver();
-    let newSnake = [...snake];
-    let snakeX = newSnake[0][0];
-    let snakeY = newSnake[0][1];
-    snakeX += velocityX;
-    snakeY += velocityY;
-
-    if (
-      snakeX < 1 ||
-      snakeX > 30 ||
-      snakeY < 1 ||
-      snakeY > 30 ||
-      newSnake.slice(1).some(([x, y]) => x === snakeX && y === snakeY)
-    ) {
-      setGameOver(true);
-      return;
-    }
-
-    newSnake.unshift([snakeX, snakeY]);
-    if (snakeX === foodX && snakeY === foodY) {
-      setScore(score + 1);
-    } else {
-      newSnake.pop();
-    }
-    setSnake(newSnake);
   };
 
   return (
